41f3e262fa700320f415275de4d7dea48a431c0f
lakshmangovinda <lakshmangovind1998@gmail.com>
2020-10-05 15:01:27 +0530

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,42 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators, FormArray, FormBuilder, Form} from '@angular/forms';
 import {UsernameValidators} from '../uservalidators';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
+export interface Element {
+  candidatename: string;
+  email: string;
+  Contact: number;
+  Team: string;
+  Type: string;
+  Experience: string;
+  Hireddate;
+  Duration: string;
+  status: string;
+  stipend: number;
+  Action;
+}
+const ELEMENT_DATA: Element[] = [
+  // tslint:disable-next-line:max-line-length
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com', Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper', Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+  { candidatename: 'lakshman', Contact: 6302569388, email: 'lakshman@example.com',  Team: 'angulardeveloper' , Type: 'unpaid', Experience: 'Fresher', Hireddate: '02/10/2020', Duration: '3 months', status: 'active', stipend: 0, Action: null},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(5)
-      , UsernameValidators.nospace]),
-    email: new FormControl('', [Validators.required, Validators.email ]),
-    phone: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10),
-      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
-    interndurtion: new FormControl('', [Validators.required]),
-    collegeaddress: new FormControl('', [Validators.required, Validators.minLength(20)]),
-    collegename: new FormControl('', [Validators.required]),
-    collegecontact: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10),
-      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
-    candidateaddress: new FormControl('', [Validators.required, Validators.minLength(20)]),
-    status: new FormControl('', [Validators.required]),
-    team: new FormControl(''),
-    payment: new FormControl(''),
-    experience: new FormControl(''),
-    pictureupload: new FormControl(''),
-    joiningletter: new FormControl(''),
-    resume: new FormControl(''),
-    noc: new FormControl(''),
-    frontaadhar: new FormControl(''),
-    backaadhar: new FormControl(''),
-    frontpan: new FormControl(''),
-    backpan: new FormControl('')
-  });
-submit(value){
-  console.log(value);
-}
+
+export class AppComponent implements AfterViewInit {
   get username(){
     return    this.form.get('name');
   }
@@ -96,6 +97,47 @@ submit(value){
   get backpan(){
   return this.form.get('backpan');
   }
+  displayedColumns = ['candidateName', 'Contact', 'Type', 'Team', 'Experience', 'Hired Date', 'Duration', 'status', 'stipend', 'Action'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(5)
+      , UsernameValidators.nospace]),
+    email: new FormControl('', [Validators.required, Validators.email ]),
+    phone: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10),
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
+    interndurtion: new FormControl('', [Validators.required]),
+    collegeaddress: new FormControl('', [Validators.required, Validators.minLength(20)]),
+    collegename: new FormControl('', [Validators.required]),
+    collegecontact: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10),
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
+    candidateaddress: new FormControl('', [Validators.required, Validators.minLength(20)]),
+    status: new FormControl('', [Validators.required]),
+    team: new FormControl(''),
+    payment: new FormControl(''),
+    experience: new FormControl(''),
+    pictureupload: new FormControl(''),
+    joiningletter: new FormControl(''),
+    resume: new FormControl(''),
+    noc: new FormControl(''),
+    frontaadhar: new FormControl(''),
+    backaadhar: new FormControl(''),
+    frontpan: new FormControl(''),
+    backpan: new FormControl('')
+  });
+fileupload: any;
+imageurl: any;
+aadhar: any;
+frontaadharurl: any;
+backaadharurl: any;
+backadhar: any;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+submit(value){
+  console.log(value);
+}
   addcontact(){
   const txtNewInputBox = document.createElement('input');
   txtNewInputBox.setAttribute('type', 'text');
@@ -104,7 +146,28 @@ submit(value){
   txtNewInputBox.setAttribute('formControlName', 'phone');
   document.getElementById('spanning').appendChild(txtNewInputBox);
 }
-preview(){
-  const file = document.getElementById('uploadbtn');
+  imagepreview(file){
+this.fileupload = file.item(0);
+const reader = new FileReader();
+reader.onload = (event: any) => {
+  this.imageurl = event.target.result;
+};
+reader.readAsDataURL(this.fileupload);
 }
+  aadharfrontpreview(file){
+    this.aadhar = file.item(0);
+    const fileread = new FileReader();
+    fileread.onload = (event: any) => {
+      this.frontaadharurl = event.target.result;
+    };
+    fileread.readAsDataURL(this.aadhar);
+  }
+  aadharbackpreview(file){
+    this.backadhar = file.item(0);
+    const fileread = new FileReader();
+    fileread.onload = (event: any) => {
+      this.backaadharurl = event.target.result;
+    };
+    fileread.readAsDataURL(this.backadhar);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule } from '@angular/material/table';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   declarations: [
@@ -25,6 +28,9 @@ import {MatIconModule} from "@angular/material/icon";
     MatInputModule,
     MatDatepickerModule,
     MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
